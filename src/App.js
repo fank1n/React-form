e4f22de459302c03ffba0b79d85a8399d78f7d64
fank1n <sanwhite98@gmail.com>
2021-10-16 21:51:49 +0300

    ready-class-form

--- src/App.js
+++ src/App.js
@@ -1,56 +1,14 @@
-import './App.css';
-import React from 'react';
-import Blank from './Blank'
-import Summary from './Summary'
+import "./App.css";
+import React from "react";
+import Blank from "./Blank";
+import Summary from "./Summary";
 
-class App extends React.Component{
-    constructor(props) {
-      super(props);
-  
-      this.state = {
-        firstName: '',
-        secondName: '',
-        date: '',
-        phoneNumber: '',
-        site: '',
-        about: '',
-        stack: '',
-        lastProj: '',
-        isSubmitted: false,
-      };
-    }
-  
-    submitForm = (e) => {
-      e.preventDefault();
-      console.log(this.state);
-      this.setState(()=>{
-        return {
-          isSubmitted: true,
-        }
-      });
-      console.log(this.state.isSubmitted)
-    };
-    
-    changeForm = ({target: {name, value}}) => {
-      this.setState((prev) => {
-          if (name === "phoneNumber" && value.length > 12) {
-              return null;
-          }
-          return {
-                  [name]: value,
-              }
-          })
-          console.log(name,value)
-      }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-      formValidation = () =>{
-        const nameRegExp = /^[А-Яа-яЁё]+$/;
-        const urlRegExp = /\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i;
-        const phoneNumberRegExp = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
-        
-      }
-    resetForm = (e) => {
-      this.setState({
+    this.state = {
+      formData: {
         firstName: "",
         secondName: "",
         date: "",
@@ -59,60 +17,338 @@ class App extends React.Component{
         about: "",
         stack: "",
         lastProj: "",
-        isEdit:false,
-      });
+      },
+      isSubmitted: false,
+      isValid: false,
+
+      errorStatus: {
+        firstName: "",
+        secondName: "",
+        date: "",
+        phoneNumber: "",
+        site: "",
+        about: "",
+        stack: "",
+        lastProj: "",
+      },
     };
-    render(){
-      const {
-        firstName,
-        secondName,
-        date,
-        phoneNumber,
-        site,
-        about,
-        stack,
-        lastProj,
-      } = this.state;
-      if(this.state.isSubmitted){
-        return (
-          <Summary
-           firstName = {firstName}
-           secondName = {secondName}
-           date = {date}
-           phoneNumber = {phoneNumber}
-           site = {site}
-           date = {date}
-           phoneNumber = {phoneNumber}
-           site = {site}
-           about = {about}
-           stack = {stack}
-           lastProj = {lastProj}
-           changeForm = {this.changeForm}
-           resetForm = {this.resetForm}
-           submitForm = {this.submitForm}
-          />
-        )
-      }
-      return(
-        <div>
-          <Blank firstName = {firstName}
-           secondName = {secondName}
-           date = {date}
-           phoneNumber = {phoneNumber}
-           site = {site}
-           date = {date}
-           phoneNumber = {phoneNumber}
-           site = {site}
-           about = {about}
-           stack = {stack}
-           lastProj = {lastProj}
-           changeForm = {this.changeForm}
-           resetForm = {this.resetForm}
-           submitForm = {this.submitForm}
-           />
-        </div>
-      )
-    }
   }
+
+  validateForm = () => {
+    const nameRegExp = /^[А-Яа-яЁё]+$/;
+    const phoneNumberRegExp =
+      /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+    const urlRegExp =
+      /\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i;
+    for (let [name, value] of Object.entries(this.state.formData)) {
+      if (value === "") {
+        this.setState((previous) => {
+          return {
+            errorStatus: {
+              ...previous.errorStatus,
+              [name]: "Поле пустое, пожалуйста заполните",
+            },
+            isValid: false,
+          };
+        });
+        return;
+      }
+      if (
+        (name === "firstName" || name === "secondName") &&
+        !nameRegExp.test(value)
+      ) {
+        this.setState((previous) => {
+          return {
+            formData: {
+              ...previous.formData,
+              [name]: value,
+            },
+            errorStatus: {
+              ...previous.errorStatus,
+              [name]: "Имя должно быть на кириллице",
+            },
+            isValid: false,
+          };
+        });
+        return;
+      }
+      if (
+        (name === "firstName" || name === "secondName") &&
+        value[0] !== value[0].toUpperCase()
+      ) {
+        this.setState((previous) => {
+          return {
+            errorStatus: {
+              ...previous.errorStatus,
+              [name]: `${name === "firstName" ? "Имя" : "Фамилия"} должн${
+                name === "firstName" ? "о" : "а"
+              } начинаться с заглавной буквы`,
+            },
+            isValid: false,
+          };
+        });
+        return;
+      }
+      if (name === "phoneNumber" && !phoneNumberRegExp.test(value)) {
+        this.setState((previous) => {
+          return {
+            formData: {
+              ...previous.formData,
+              [name]: value,
+            },
+
+            errorStatus: {
+              ...previous.errorStatus,
+              [name]: "Не соответствие формату. Формат: +X XXX XX XX",
+            },
+            isValid: false,
+          };
+        });
+        return;
+      }
+      if (name === "site" && !urlRegExp.test(value)) {
+        this.setState((previous) => {
+          return {
+            formData: {
+              ...previous.formData,
+              [name]: value,
+            },
+
+            errorStatus: {
+              ...previous.errorStatus,
+              [name]: "Не соответствие на формат. Формат: https://...",
+            },
+            isValid: false,
+          };
+        });
+        return;
+      }
+      this.setState((previous) => {
+        return {
+          isValid: true,
+        };
+      });
+    }
+  };
+
+  submitForm = (e) => {
+    e.preventDefault();
+    this.validateForm();
+    this.setState(() => {
+      return {
+        isSubmitted: true,
+      };
+    });
+  };
+
+  changeForm = ({ target: { name, value } }) => {
+    const nameRegExp = /^[А-Яа-яЁё]+$/;
+    const phoneNumberRegExp =
+      /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+    const urlRegExp =
+      /\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i;
+    this.setState((previous) => {
+      if (
+        (name === "firstName" || name === "secondName") &&
+        !nameRegExp.test(value)
+      ) {
+        return {
+          formData: {
+            ...previous.formData,
+            [name]: value,
+          },
+
+          errorStatus: {
+            ...previous.errorStatus,
+            [name]: "Имя должно быть на кириллице",
+          },
+        };
+      }
+
+      if (name === "phoneNumber" && value.length > 12) {
+        return null;
+      }
+
+      if (
+        (name === "about" || name === "stack" || name === "lastProj") &&
+        value.length > 600
+      ) {
+        return {
+          formData: {
+            ...previous.formData,
+            [name]: value.slice(0, 600),
+          },
+
+          errorStatus: {
+            ...previous.errorStatus,
+            [name]: "Превышен лимит знаков",
+          },
+        };
+      }
+
+      if (
+        (name === "about" || name === "stack" || name === "lastProj") &&
+        value.length < 600
+      ) {
+        return {
+          formData: {
+            ...previous.formData,
+            [name]: value,
+          },
+
+          errorStatus: {
+            ...previous.errorStatus,
+            [name]: "",
+          },
+        };
+      }
+      if (name === "phoneNumber" && !phoneNumberRegExp.test(value)) {
+        return {
+          formData: {
+            ...previous.formData,
+            [name]: value,
+          },
+
+          errorStatus: {
+            ...previous.errorStatus,
+            [name]: "Не соответствие формату. Формат: +X XXX XX XX",
+          },
+        };
+      }
+      if (name === "site" && !urlRegExp.test(value)) {
+        return {
+          formData: {
+            ...previous.formData,
+            [name]: value,
+          },
+
+          errorStatus: {
+            ...previous.errorStatus,
+            [name]: "Не соответствие на формат. Формат: https://...",
+          },
+        };
+      }
+      return {
+        formData: {
+          ...previous.formData,
+          [name]: value,
+        },
+
+        errorStatus: {
+          ...previous.errorStatus,
+          [name]: "",
+        },
+      };
+    });
+  };
+
+  handlerBlur = ({ target: { name, value } }) => {
+    this.setState((previous) => {
+      if (value === "") {
+        return {
+          errorStatus: {
+            ...previous.errorStatus,
+            [name]: "Поле пустое, пожалуйста заполните",
+          },
+        };
+      }
+      if (
+        (name === "firstName" || name === "secondName") &&
+        value[0] !== value[0].toUpperCase()
+      ) {
+        return {
+          errorStatus: {
+            ...previous.errorStatus,
+            [name]: `${name === "firstName" ? "Имя" : "Фамилия"} должн${
+              name === "firstName" ? "о" : "а"
+            } начинаться с заглавной буквы`,
+          },
+        };
+      }
+    });
+  };
+
+  resetForm = (e) => {
+    this.setState({
+      firstName: "",
+      secondName: "",
+      date: "",
+      phoneNumber: "",
+      site: "",
+      about: "",
+      stack: "",
+      lastProj: "",
+      isSubmitted: false,
+      isValid: false,
+
+      errorStatus: {
+        firstName: "",
+        secondName: "",
+        date: "",
+        phoneNumber: "",
+        site: "",
+        about: "",
+        stack: "",
+        lastProj: "",
+      },
+    });
+  };
+
+  render() {
+    const {
+      firstName,
+      secondName,
+      date,
+      phoneNumber,
+      site,
+      about,
+      stack,
+      lastProj,
+    } = this.state.formData;
+
+    if (this.state.isSubmitted && this.state.isValid) {
+      return (
+        <Summary
+          firstName={firstName}
+          secondName={secondName}
+          date={date}
+          phoneNumber={phoneNumber}
+          site={site}
+          date={date}
+          phoneNumber={phoneNumber}
+          site={site}
+          about={about}
+          stack={stack}
+          lastProj={lastProj}
+          changeForm={this.changeForm}
+          resetForm={this.resetForm}
+          submitForm={this.submitForm}
+        />
+      );
+    }
+    return (
+      <div>
+        <Blank
+          firstName={firstName}
+          secondName={secondName}
+          date={date}
+          phoneNumber={phoneNumber}
+          site={site}
+          date={date}
+          phoneNumber={phoneNumber}
+          site={site}
+          about={about}
+          stack={stack}
+          lastProj={lastProj}
+          changeForm={this.changeForm}
+          resetForm={this.resetForm}
+          submitForm={this.submitForm}
+          errorStatus={this.state.errorStatus}
+          handlerBlur={this.handlerBlur}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/Blank.js
+++ src/Blank.js
@@ -1,65 +1,7 @@
 import React from "react";
 
 class Blank extends React.Component {
-
-  //   this.state = {
-  //     firstName: undefined,
-  //     secondName: undefined,
-  //     date: undefined,
-  //     phoneNumber: undefined,
-  //     site: undefined,
-  //     about: undefined,
-  //     stack: undefined,
-  //     lastProj: undefined,
-  //     isEdit: false,
-  //   };
-  // }
-  // change = (e) => {
-  //   const { name, value } = e.target;
-  //   this.setState({ [name]: value });
-  // };
-
-  // submitForm = (e) => {
-  //   e.preventDefault();
-  //   console.log(this.state);
-  //   this.setState({
-  //       firstName: this.state.firstName,
-  //       secondName: this.state.secondName,
-  //       date: this.state.date,
-  //       phoneNumber: this.state.phoneNumber,
-  //       site: this.state.site,
-  //       about: this.state.about,
-  //       stack: this.state.stack,
-  //       lastProj: this.state.lastProj,
-  //       isEdit: true,
-  //   });
-    
-  // };
-
-
-  // resetForm = (e) => {
-  //   this.setState({
-  //     firstName: "",
-  //     secondName: "",
-  //     date: "",
-  //     phoneNumber: "",
-  //     site: "",
-  //     about: "",
-  //     stack: "",
-  //     lastProj: "",
-  //     isEdit:false,
-  //   });
-  // };
-
-//   validation = (values) => {
-//       let errors = {};
-//       this.if(this.state.firstName === ''){
-
-//       }
-//   }
-
-//   if(this.state.firstName === ''){
-//       errors.this.state.firstName
+  
   render() {
     const maxTextLength = 600;
     return (
@@ -75,25 +17,45 @@ class Blank extends React.Component {
             value={this.props.firstName}
             onChange={this.props.changeForm}
             className="input"
+            onBlur = {this.props.handlerBlur}
           />
+          {this.props.errorStatus.firstName ? <div className = "errorMessage">{this.props.errorStatus.firstName}</div> : ''}
           <label className="label">Фамилия:</label>
           <input
             type="text"
             name="secondName"
             value={this.props.secondName}
             onChange={this.props.changeForm}
+            onBlur = {this.props.handlerBlur}
           />
+          {this.props.errorStatus.secondName ? <div className = "errorMessage">{this.props.errorStatus.secondName}</div> : ''}
           <label className="label">Дата рождения:</label>
-          <input type="date" name="date" value={this.props.date} onChange={this.props.changeForm}/>
+          <input 
+            type="date" 
+            name="date" 
+            value={this.props.date} 
+            onChange={this.props.changeForm}
+            onBlur = {this.props.handlerBlur}
+          />
+          {this.props.errorStatus.date ? <div className = "errorMessage">{this.props.errorStatus.date}</div> : ''}
           <label className="label">Телефон:</label>
           <input
-            type="number"
+            type="text"
             name="phoneNumber"
             value={this.props.phoneNumber}
             onChange={this.props.changeForm}
+            onBlur = {this.props.handlerBlur}
           />
+          {this.props.errorStatus.phoneNumber ? <div className = "errorMessage">{this.props.errorStatus.phoneNumber}</div> : ''}
           <label className="label">Сайт:</label>
-          <input type="text" name="site" value={this.props.site} onChange={this.props.changeForm}/>
+          <input 
+          type="text" 
+          name="site" 
+          value={this.props.site} 
+          onChange={this.props.changeForm}
+          onBlur = {this.props.handlerBlur}
+          />
+          {this.props.errorStatus.site ? <div className = "errorMessage">{this.props.errorStatus.site}</div> : ''}
           <label className="label">О себе:</label>
           <textarea
             cols="30"
@@ -101,8 +63,11 @@ class Blank extends React.Component {
             value={this.props.about}
             name="about"
             onChange={this.props.changeForm}
+            onBlur = {this.props.handlerBlur}
           />
-          <p className = 'symbols'>{maxTextLength - this.props.about.length}{'/600'}</p>
+          {this.props.errorStatus.about 
+          ? <div className = "errorMessage">{this.props.errorStatus.about}</div> 
+          : <p className = 'symbols'>{maxTextLength - this.props.about.length}{'/600'}</p>}
           <label className="label">Стек технологий:</label>
           <textarea
             cols="30"
@@ -110,8 +75,11 @@ class Blank extends React.Component {
             value={this.props.stack}
             name="stack"
             onChange={this.props.changeForm}
+            onBlur = {this.props.handlerBlur}
           />
-          <p className = 'symbols'>{maxTextLength - this.props.stack.length}{'/600'}</p>
+          {this.props.errorStatus.stack 
+          ? <div className = "errorMessage">{this.props.errorStatus.stack}</div> 
+          : <p className = 'symbols'>{maxTextLength - this.props.stack.length}{'/600'}</p>}
           <label className="label">Описание последнего проекта:</label>
           <textarea
             cols="30"
@@ -119,8 +87,11 @@ class Blank extends React.Component {
             value={this.props.lastProj}
             name="lastProj"
             onChange={this.props.changeForm}
+            onBlur = {this.props.handlerBlur}
           />
-          <p className = 'symbols'>{maxTextLength - this.props.lastProj.length}{'/600'}</p>
+          {this.props.errorStatus.lastProj 
+          ? <div className = "errorMessage">{this.props.errorStatus.lastProj}</div> 
+          : <p className = 'symbols'>{maxTextLength - this.props.lastProj.length}{'/600'}</p>}
           <button type = "reset" id="cancel-btn" className="default-button">
             Отменить
           </button>
