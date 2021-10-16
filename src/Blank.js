5f771a39fd048cabace50a8da2a50ab6336b812d
fank1n <sanwhite98@gmail.com>
2021-10-16 23:25:06 +0300

    function-form-component

--- src/Blank.js
+++ src/Blank.js
@@ -1,107 +1,155 @@
 import React from "react";
 
-class Blank extends React.Component {
-  
-  render() {
-    const maxTextLength = 600;
-    return (
-      <div id="container">
-        <header>
-          <p id="headerName">Создание анкеты</p>
-        </header>
-        <form onSubmit = {this.props.submitForm} onReset = {this.props.resetForm} id="form">
-          <label className="label">Имя:</label>
-          <input
-            type="text"
-            name="firstName"
-            value={this.props.firstName}
-            onChange={this.props.changeForm}
-            className="input"
-            onBlur = {this.props.handlerBlur}
-          />
-          {this.props.errorStatus.firstName ? <div className = "errorMessage">{this.props.errorStatus.firstName}</div> : ''}
-          <label className="label">Фамилия:</label>
-          <input
-            type="text"
-            name="secondName"
-            value={this.props.secondName}
-            onChange={this.props.changeForm}
-            onBlur = {this.props.handlerBlur}
-          />
-          {this.props.errorStatus.secondName ? <div className = "errorMessage">{this.props.errorStatus.secondName}</div> : ''}
-          <label className="label">Дата рождения:</label>
-          <input 
-            type="date" 
-            name="date" 
-            value={this.props.date} 
-            onChange={this.props.changeForm}
-            onBlur = {this.props.handlerBlur}
-          />
-          {this.props.errorStatus.date ? <div className = "errorMessage">{this.props.errorStatus.date}</div> : ''}
-          <label className="label">Телефон:</label>
-          <input
-            type="text"
-            name="phoneNumber"
-            value={this.props.phoneNumber}
-            onChange={this.props.changeForm}
-            onBlur = {this.props.handlerBlur}
-          />
-          {this.props.errorStatus.phoneNumber ? <div className = "errorMessage">{this.props.errorStatus.phoneNumber}</div> : ''}
-          <label className="label">Сайт:</label>
-          <input 
-          type="text" 
-          name="site" 
-          value={this.props.site} 
-          onChange={this.props.changeForm}
-          onBlur = {this.props.handlerBlur}
-          />
-          {this.props.errorStatus.site ? <div className = "errorMessage">{this.props.errorStatus.site}</div> : ''}
-          <label className="label">О себе:</label>
-          <textarea
-            cols="30"
-            rows="4"
-            value={this.props.about}
-            name="about"
-            onChange={this.props.changeForm}
-            onBlur = {this.props.handlerBlur}
-          />
-          {this.props.errorStatus.about 
-          ? <div className = "errorMessage">{this.props.errorStatus.about}</div> 
-          : <p className = 'symbols'>{maxTextLength - this.props.about.length}{'/600'}</p>}
-          <label className="label">Стек технологий:</label>
-          <textarea
-            cols="30"
-            rows="4"
-            value={this.props.stack}
-            name="stack"
-            onChange={this.props.changeForm}
-            onBlur = {this.props.handlerBlur}
-          />
-          {this.props.errorStatus.stack 
-          ? <div className = "errorMessage">{this.props.errorStatus.stack}</div> 
-          : <p className = 'symbols'>{maxTextLength - this.props.stack.length}{'/600'}</p>}
-          <label className="label">Описание последнего проекта:</label>
-          <textarea
-            cols="30"
-            rows="4"
-            value={this.props.lastProj}
-            name="lastProj"
-            onChange={this.props.changeForm}
-            onBlur = {this.props.handlerBlur}
-          />
-          {this.props.errorStatus.lastProj 
-          ? <div className = "errorMessage">{this.props.errorStatus.lastProj}</div> 
-          : <p className = 'symbols'>{maxTextLength - this.props.lastProj.length}{'/600'}</p>}
-          <button type = "reset" id="cancel-btn" className="default-button">
-            Отменить
-          </button>
-          <button type="submit" id="submit-btn" className="default-button">
-            Сохранить
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+const Blank = ({
+  formData: {
+    firstName,
+    secondName,
+    date,
+    phoneNumber,
+    site,
+    about,
+    stack,
+    lastProj,
+  },
+  changeForm,
+  resetForm,
+  submitForm,
+  errorStatus,
+  handlerBlur,
+}) => {
+  const maxTextLength = 600;
+  return (
+    <div id="container">
+      <header>
+        <p id="headerName">Создание анкеты</p>
+      </header>
+      <form onSubmit={submitForm} onReset={resetForm} id="form">
+        <label className="label">Имя:</label>
+        <input
+          type="text"
+          name="firstName"
+          value={firstName}
+          onChange={changeForm}
+          className="input"
+          onBlur={handlerBlur}
+        />
+        {errorStatus.firstName ? (
+          <div className="errorMessage">{errorStatus.firstName}</div>
+        ) : (
+          ""
+        )}
+        <label className="label">Фамилия:</label>
+        <input
+          type="text"
+          name="secondName"
+          value={secondName}
+          onChange={changeForm}
+          onBlur={handlerBlur}
+        />
+        {errorStatus.secondName ? (
+          <div className="errorMessage">{errorStatus.secondName}</div>
+        ) : (
+          ""
+        )}
+        <label className="label">Дата рождения:</label>
+        <input
+          type="date"
+          name="date"
+          value={date}
+          onChange={changeForm}
+          onBlur={handlerBlur}
+        />
+        {errorStatus.date ? (
+          <div className="errorMessage">{errorStatus.date}</div>
+        ) : (
+          ""
+        )}
+        <label className="label">Телефон:</label>
+        <input
+          type="text"
+          name="phoneNumber"
+          value={phoneNumber}
+          onChange={changeForm}
+          onBlur={handlerBlur}
+        />
+        {errorStatus.phoneNumber ? (
+          <div className="errorMessage">{errorStatus.phoneNumber}</div>
+        ) : (
+          ""
+        )}
+        <label className="label">Сайт:</label>
+        <input
+          type="text"
+          name="site"
+          value={site}
+          onChange={changeForm}
+          onBlur={handlerBlur}
+        />
+        {errorStatus.site ? (
+          <div className="errorMessage">{errorStatus.site}</div>
+        ) : (
+          ""
+        )}
+        <label className="label">О себе:</label>
+        <textarea
+          cols="30"
+          rows="4"
+          value={about}
+          name="about"
+          onChange={changeForm}
+          onBlur={handlerBlur}
+        />
+        {errorStatus.about ? (
+          <div className="errorMessage">{errorStatus.about}</div>
+        ) : (
+          <p className="symbols">
+            {maxTextLength - about.length}
+            {"/600"}
+          </p>
+        )}
+        <label className="label">Стек технологий:</label>
+        <textarea
+          cols="30"
+          rows="4"
+          value={stack}
+          name="stack"
+          onChange={changeForm}
+          onBlur={handlerBlur}
+        />
+        {errorStatus.stack ? (
+          <div className="errorMessage">{errorStatus.stack}</div>
+        ) : (
+          <p className="symbols">
+            {maxTextLength - stack.length}
+            {"/600"}
+          </p>
+        )}
+        <label className="label">Описание последнего проекта:</label>
+        <textarea
+          cols="30"
+          rows="4"
+          value={lastProj}
+          name="lastProj"
+          onChange={changeForm}
+          onBlur={handlerBlur}
+        />
+        {errorStatus.lastProj ? (
+          <div className="errorMessage">{errorStatus.lastProj}</div>
+        ) : (
+          <p className="symbols">
+            {maxTextLength - lastProj.length}
+            {"/600"}
+          </p>
+        )}
+        <button type="reset" id="cancel-btn" className="default-button">
+          Отменить
+        </button>
+        <button type="submit" id="submit-btn" className="default-button">
+          Сохранить
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Blank;
--- src/Summary.js
+++ src/Summary.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React from "react";
 
-class Summary extends React.Component{
-    render(){
-        return(
-            <div className = "summary-container">
-                <div className = 'summary-header'><p className = "summary-header-name">Анкета</p></div>
-                <p className = 'summary-data'><span>Имя:</span>{this.props.firstName}</p>
-                <p className = 'summary-data'><span>Фамилия:</span>{this.props.secondName}</p>
-                <p className = 'summary-data'><span>Дата рождения:</span>{this.props.date}</p>
-                <p className = 'summary-data'><span>Телефон:</span>{this.props.phoneNumber}</p>
-                <p className = 'summary-data'><span>Сайт:</span>{this.props.site}</p>
-                <span className = 'summary-data'>О себе:</span>
-                <p className = 'summary-data'>{this.props.about}</p>
-                <span className = 'summary-data'>Стэк технологий:</span>
-                <p className = 'summary-data'>{this.props.stack}</p>
-                <span className = 'summary-data'>Описание последнего проекта:</span>
-                <p className = 'summary-data'>{this.props.lastProj}</p>
-            </div>
-        )
-    }
-}
+const Summary = ({
+  formData: {
+    firstName,
+    secondName,
+    date,
+    phoneNumber,
+    site,
+    about,
+    stack,
+    lastProj,
+  },
+}) => {
+  return (
+    <div className="summary-container">
+      <div className="summary-header">
+        <p className="summary-header-name">Анкета</p>
+      </div>
+      <p className="summary-data">
+        <span>Имя:</span>
+        {firstName}
+      </p>
+      <p className="summary-data">
+        <span>Фамилия:</span>
+        {secondName}
+      </p>
+      <p className="summary-data">
+        <span>Дата рождения:</span>
+        {date}
+      </p>
+      <p className="summary-data">
+        <span>Телефон:</span>
+        {phoneNumber}
+      </p>
+      <p className="summary-data">
+        <span>Сайт:</span>
+        {site}
+      </p>
+      <span className="summary-data">О себе:</span>
+      <p className="summary-data">{about}</p>
+      <span className="summary-data">Стэк технологий:</span>
+      <p className="summary-data">{stack}</p>
+      <span className="summary-data">Описание последнего проекта:</span>
+      <p className="summary-data">{lastProj}</p>
+    </div>
+  );
+};
 
 export default Summary;
